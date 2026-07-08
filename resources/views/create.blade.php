2e497194822c9d1f7f255f0c687c82bbe6caf8ac
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-08 16:57:40 +0100

    styling create view

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,38 +1,44 @@
-<h1>Create Student</h1>
+@extends('layout')
+
+@section('content')
+<h4>Create Student</h4>
 @if($errors->any())
+    <div class="py-4 alert alert-danger">
         <ul>
         @foreach ($errors->all() as $err )
             <li>{{ $err }}</li>
         @endforeach
         </ul>
+    </div>
 @endif
-<form method="post" action="{{ route('students.store') }}" enctype="multipart/form-data">
-    <div>
-        <label for="name">Name</label>
-        <input id="name" type="text" name="name" placeholder="Student Name" value="{{ old('name') }}" />
+<form class="w-75" method="post" action="{{ route('students.store') }}" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label class="form-label" for="name">Name</label>
+        <input class="form-control" id="name" type="text" name="name" placeholder="Student Name" value="{{ old('name') }}" />
     </div>
-    <div>
-        <label for="email">Email</label>
-        <input id="email" type="email" name="email" placeholder="Student Email" value="{{ old('email') }}" />
+    <div class="mb-3">
+        <label class="form-label" for="email">Email</label>
+        <input class="form-control" id="email" type="email" name="email" placeholder="Student Email" value="{{ old('email') }}" />
     </div>
-    <div>
-        <label for="phone">Phone</label>
-        <input id="phone" type="text" name="phone" placeholder="Student Phone" value="{{ old('phone') }}" />
+    <div class="mb-3">
+        <label class="form-label" for="phone">Phone</label>
+        <input class="form-control" id="phone" type="text" name="phone" placeholder="Student Phone" value="{{ old('phone') }}" />
     </div>
-    <div>
-        <label for="Section">Section</label>
-        <input id="section" type="text" name="section" placeholder="Studnet Section" value="{{ old('section') }}" />
+    <div class="mb-3">
+        <label class="form-label" for="Section">Section</label>
+        <input class="form-control" id="section" type="text" name="section" placeholder="Studnet Section" value="{{ old('section') }}" />
     </div>
-    <div>
-        <label for="description">Description</label>
-        <textarea id="description" name="description">{{ @old('description') }}</textarea>
+    <div class="mb-3">
+        <label class="form-label" for="description">Description</label>
+        <textarea class="form-control" id="description" name="description">{{ @old('description') }}</textarea>
     </div>
-    <div>
-        <label for="image">Image</label>
-        <input id="image" type="file" name="image" />
+    <div class="mb-3">
+        <label class="form-label" for="image">Image</label>
+        <input class="form-control" id="image" type="file" name="image" />
     </div>
-    <div>
-        <a href="{{ route('students.index') }}">Cancel</a>
-        <button type="submit">Add Student</button>
+    <div class="mb-3">
+        <a class="btn btn-outline-primary me-3" href="{{ route('students.index') }}">Cancel</a>
+        <button class="btn btn-primary" type="submit">Add Student</button>
     </div>
 </form>
+@endsection
